ef142886781807384470396b5e4e8a6bbcb411b9
Anup81 <anupdhakal81@gmail.com>
2022-05-29 11:05:50 +0545

    Getting All Users data from API and Setting UP

--- client/src/components/AllUsers.jsx
+++ client/src/components/AllUsers.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableHead,
+} from "@mui/material";
+
+import { getUsers } from "../service/api";
 
 const AllUsers = () => {
-  return <div>Hello from All Users</div>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  const getAllUsers = async () => {
+    let response = await getUsers();
+    setUsers(response.data);
+    console.log(response.data);
+  };
+
+  return (
+    <div>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Username</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Phone</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell>{user._id}</TableCell>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.username}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.phone}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default AllUsers;
